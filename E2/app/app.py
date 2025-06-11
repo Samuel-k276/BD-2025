b4ac1ea34113c2a5f0240be33ab9d52815753185
dinis-sales <dinissales2005@gmail.com>
2025-06-11 16:58:47 +0100

    check_in

--- E2/app/app.py
+++ E2/app/app.py
@@ -72,9 +72,6 @@ def list_flights_from_departure(partida):
 
 
 
-
-
-
 @app.route('/voos/<partida>/<chegada>', methods=['GET'])
 def list_flights(partida, chegada):
    try:
@@ -138,9 +135,68 @@ def make_purchhase(voo):
    
 
 
-@app.route('/checkin/<bilhete>', methods=['GET'])
-def check_in(bilhete):
-   return jsonify(f"Check-in for ticket {bilhete} successful"	)
+@app.route('/checkin/<bilhete>', methods=['POST'])
+def check_in(bilhete_id):
+   try:
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+
+               cur.execute("""
+                   SELECT b.id, b.voo_id, b.no_serie, b.classe, b.lugar
+                   FROM bilhete b
+                   WHERE b.id = %s
+               """, (bilhete_id,))
+               bilhete = cur.fetchone()
+               # Verificar se o bilhete existe e se já tem assento
+               if bilhete is None:
+                  return jsonify({"error": "Bilhete não encontrado"}), 404
+
+               if bilhete[4] is not None:
+                  return jsonify({"error": "Check-in já feito"}), 400
+
+               #voo_id = bilhete[1]
+               no_serie = bilhete[2]
+               classe = bilhete[3]
+
+                # Procurar um assento disponível da classe correspondente
+               cur.execute("""
+                  SELECT a.lugar
+                  FROM assento a
+                  WHERE a.no_serie = %s
+                     AND a.classe = %s
+                     AND NOT EXISTS (
+                        SELECT 1 FROM bilhete b
+                        WHERE b.no_serie = a.no_serie
+                           AND b.lugar = a.lugar
+                     )
+                  LIMIT 1
+               """, (no_serie, classe))
+               assento_livre = cur.fetchone()
+
+               if assento_livre is None:
+                  return jsonify({"error": "Sem lugares disponíveis nessa classe"}), 400
+
+               lugar_atribuido = assento_livre[0]
+
+                # Atualizar o bilhete com o lugar atribuído
+               cur.execute("""
+                  UPDATE bilhete
+                  SET lugar = %s
+                  WHERE id = %s
+               """, (lugar_atribuido, bilhete_id))
+
+               conn.commit()
+
+               return jsonify({
+                  "message": "Check-in efetuado com sucesso",
+                  "lugar_atribuido": lugar_atribuido
+               }), 200
+
+   except Exception as e:
+      return jsonify({"error": str(e)}), 500
+
+               
+   
 
 
 if __name__ == '__main__':
