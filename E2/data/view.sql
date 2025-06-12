46b4f35c1656d17e3c58c304f3ccd2c409839cd8
Samuel Gomes <samuel.gomes.k276@gmail.com>
2025-06-12 17:50:08 +0100

    view

--- E2/data/view.sql
+++ E2/data/view.sql
@@ -6,7 +6,6 @@ CREATE MATERIALIZED VIEW estatisticas_voos AS
 SELECT
     v.no_serie,
     v.hora_partida,
-
     ap.cidade AS cidade_partida,
     ap.pais AS pais_partida,
     ac.cidade AS cidade_chegada,
@@ -15,16 +14,18 @@ SELECT
     EXTRACT(MONTH FROM v.hora_partida) AS mes,
     EXTRACT(DAY FROM v.hora_partida) AS dia_do_mes,
     TRIM(TO_CHAR(v.hora_partida, 'Day')) AS dia_da_semana,
-
+    
     COUNT(CASE WHEN b.prim_classe = TRUE THEN 1 END) AS passageiros_1c,
     COUNT(CASE WHEN b.prim_classe = FALSE THEN 1 END) AS passageiros_2c,
-
-    SUM(CASE WHEN b.prim_classe = TRUE THEN b.preco END) AS vendas_1c,
-    SUM(CASE WHEN b.prim_classe = FALSE THEN b.preco END) AS vendas_2c
-
+    
+    (SELECT COUNT(*) FROM assento a WHERE a.no_serie = v.no_serie AND a.prim_classe = TRUE) AS assentos_1c,
+    (SELECT COUNT(*) FROM assento a WHERE a.no_serie = v.no_serie AND a.prim_classe = FALSE) AS assentos_2c,
+    
+    SUM(CASE WHEN b.prim_classe = TRUE THEN b.preco ELSE 0 END) AS vendas_1c,
+    SUM(CASE WHEN b.prim_classe = FALSE THEN b.preco ELSE 0 END) AS vendas_2c
+    
 FROM voo v
 JOIN aeroporto ap ON v.partida = ap.codigo
 JOIN aeroporto ac ON v.chegada = ac.codigo
-JOIN bilhete b ON v.id = b.voo_id
-
-GROUP BY (v.no_serie, v.hora_partida, ap.cidade, ap.pais, ac.cidade, ac.pais)
+LEFT JOIN bilhete b ON v.id = b.voo_id
+GROUP BY v.no_serie, v.hora_partida, ap.cidade, ap.pais, ac.cidade, ac.pais;
