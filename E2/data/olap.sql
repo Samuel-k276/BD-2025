02eff2ddcc84289e98f78ed2f0975b4744cf714e
Samuel Gomes <samuel.gomes.k276@gmail.com>
2025-06-13 18:44:27 +0100

    olap feito

--- E2/data/olap.sql
+++ E2/data/olap.sql
@@ -1,19 +1,20 @@
 --ex1
-
-SELECT
-  --o least e o greatest sao usados para garantir sempre a mesma ordem alfabetica 
-  LEAST(cidade_partida, cidade_chegada) AS cidade1, 
-  GREATEST(cidade_partida, cidade_chegada) AS cidade2,
-  SUM(passageiros_1c + passageiros_2c)::float / SUM(assentos_1c + assentos_2c) AS taxa_ocupacao
-FROM estatisticas_voos
-WHERE hora_partida >= CURRENT_DATE - INTERVAL '1 year'
-GROUP BY cidade1, cidade2
-HAVING SUM(assentos_1c + assentos_2c) > 0
-ORDER BY taxa_ocupacao DESC
-LIMIT 1;
+SELECT cidade1, cidade2, taxa_ocupacao
+FROM (
+  SELECT
+    LEAST(cidade_partida, cidade_chegada) AS cidade1,
+    GREATEST(cidade_partida, cidade_chegada) AS cidade2,
+    AVG((passageiros_1c + passageiros_2c)::float / (assentos_1c + assentos_2c)) AS taxa_ocupacao,
+    RANK() OVER (ORDER BY AVG((passageiros_1c + passageiros_2c)::float / (assentos_1c + assentos_2c)) DESC) AS ranking
+  FROM estatisticas_voos
+  WHERE hora_partida >= CURRENT_DATE - INTERVAL '1 year'
+  GROUP BY 
+    LEAST(cidade_partida, cidade_chegada),
+    GREATEST(cidade_partida, cidade_chegada)
+) AS rotas_ranking
+WHERE ranking = 1;
 
 --ex2
-
 SELECT
     LEAST(cidade_partida, cidade_chegada) AS cidade1,
     GREATEST(cidade_partida, cidade_chegada) AS cidade2
@@ -29,7 +30,6 @@ HAVING COUNT(DISTINCT no_serie) = (
 )
 
 --ex3
-
 SELECT
   pais_partida,
   cidade_partida,
@@ -55,3 +55,30 @@ ORDER BY
   mes NULLS LAST,
   dia_do_mes NULLS LAST;
 
+
+--ex4
+SELECT
+  pais_partida,
+  cidade_partida,
+  pais_chegada,
+  cidade_chegada,
+  dia_da_semana,
+  SUM(passageiros_1c) AS total_1c,
+  SUM(passageiros_2c) AS total_2c,
+  ROUND(
+    SUM(passageiros_1c)::numeric / NULLIF(SUM(passageiros_2c), 0),
+    2
+  ) AS ratio_1c_2c
+
+FROM estatisticas_voos
+
+GROUP BY
+  ROLLUP((pais_partida, pais_chegada), (cidade_partida, cidade_chegada)),
+  dia_da_semana
+
+ORDER BY
+  pais_partida NULLS LAST,
+  cidade_partida NULLS LAST,
+  pais_chegada NULLS LAST,
+  cidade_chegada NULLS LAST,
+  POSITION(dia_da_semana IN 'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday');
