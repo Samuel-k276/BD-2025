0ac4c9ed3c7df807cf0b72aaff5d8106b2c1eb4b
Samuel Gomes <samuel.gomes.k276@gmail.com>
2025-06-15 22:11:31 +0100

    explicaco pratica

--- E2/data/index.sql
+++ E2/data/index.sql
@@ -18,15 +18,20 @@ ON estatisticas_voos (
     vendas_2c
 );
 
---Justificação teórica:
---Para otimizar o desempenho da vista materializada "estatisticas_voos", foram criados dois índices:
+-- Justificação teórica:
+-- Para otimizar o desempenho da vista materializada "estatisticas_voos", foram criados dois índices:
 
 -- 1. "idx_hora_partida": Este índice melhora a eficiência das consultas que filtram ou ordenam por 
---"hora_partida", uma coluna frequentemente utilizada, nomeadamente na consulta 5.2 e 5.3. No caso,
--- se a base de dados tivesse sido inicializada de maneira diferente, seria possível ver também uma
--- maior eficiência na consulta 5.1. Esta B-tree index, que é o tipo padrão de para colunas do tipo "timestamp",
---e funciona bem para consultas que envolvem intervalos de tempo.
+-- "hora_partida", uma coluna frequentemente utilizada, nomeadamente na consulta 5.1 e 5.2. 
+-- Esta B-tree index, que é o tipo padrão de para colunas do tipo "timestamp",
+-- e funciona bem para consultas que envolvem intervalos de tempo.
+-- Antes de criar este índice, as consultas 5.1 e 5.2 tinham custo de 2709 e 6823, respectivamente 
+-- e demoravam respetivamente 18ms e 23ms. Depois de criar o índice, o custo das mesmas consultas
+-- foi reduzido para 1813 e 4741, com tempos de execução de 15ms e 14ms, mostrando uma melhoria significativa, na ordem dos 30%.
 
---2. "idx_estatisticas_rollup_ordenado": índice composto que é útil para acelerar as operações de agregação (SUMS)
--- e agrupamento com ROLLUP, como na consulta 5.3. Para além disto, facilita o ORDER BY porque as colunas ficam ordenadas 
--- no índice, evitando operações adicionais de ordenação.
+-- 2. "idx_estatisticas_rollup_ordenado": índice composto que é útil para acelerar as operações de agregação (SUMS)
+-- e agrupamento com ROLLUP, como na consulta 5.3. Para além disto, facilita o ORDER BY porque as colunas ficam ordenadas
+-- no índice, evitando operações adicionais de ordenação. Antes de criar este índice, a consulta 5.3
+-- tinha um custo de 9683 e demorava 481ms. Depois de criar o índice, o custo da consulta foi reduzido para 6855,
+-- com um tempo de execução de 295ms, mostrando uma melhoria significativa, na ordem dos 40%, especialmente devido ao
+-- evitar por completo a ordenação dos resultados.
